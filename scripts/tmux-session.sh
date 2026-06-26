8ca2ccc708f0248532d0bf5347c39d56e3d80293
Eric Muehlstein <muehlbucks@gmail.com>
2026-06-26 13:26:38 -0500

    nomadnet: run as systemd daemon service; tmux shows logs instead of TUI

--- scripts/tmux-session.sh
+++ scripts/tmux-session.sh
@@ -16,7 +16,7 @@ tmux new-window -t "$SESSION:" -n lxmd
 tmux send-keys -t "$SESSION:lxmd" "journalctl -fu lxmd.service" Enter
 
 tmux new-window -t "$SESSION:" -n nomadnet
-tmux send-keys -t "$SESSION:nomadnet" "sudo -u rns env HOME=/home/rns PATH=/home/rns/.local/bin:/usr/local/bin:/usr/bin:/bin TERM=xterm-256color nomadnet --config /etc/reticulum" Enter
+tmux send-keys -t "$SESSION:nomadnet" "journalctl -fu nomadnet.service" Enter
 
 tmux new-window -t "$SESSION:" -n shell
 tmux select-window -t "$SESSION:nomadnet"
